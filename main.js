d5d437caa940ab9cf276e89d7b680dff2d05c764
unknown <yisehakmitiku@gmail.com>
2021-10-07 04:29:05 +0800

    Fix linter errors

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const closeButton = document.querySelector('.close-icon');
 const cover = document.querySelector('.desk-version');
 const listItems = document.querySelectorAll('.items');
 const popButton = document.querySelectorAll('.pop-buttons');
-const popupCover = document.querySelector('.popup-section');
 
 menuButton.addEventListener('click', () => {
   cover.classList.add('d-flex');
@@ -19,7 +18,7 @@ listItems.forEach((element) => {
   });
 });
 
-let projectCards = [
+const projectCards = [
   {
     title: 'Keeping track of hundreds of components',
     image: './IMG/modal-popup.png',
@@ -27,13 +26,13 @@ let projectCards = [
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dumm. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dumm',
     button: 'See Project',
     links: ['https://jeffkay16.github.io/Portfolio-Website/', 'https://github.com/JEFFKAY16/Portfolio-Website.git']
-  },
+}
 ];
   
-  const [project] = projectCards;
-  const popupSection = document.querySelector('.popup-section');
-  popupSection.innerHTML =` 
-  <div class="popup-card">
+  const[project] = projectCards;
+const popupSection = document.querySelector('.popup-section');
+popupSection.innerHTML = ` 
+<div class="popup-card">
     <i class="fa fa-times popup-close-btn" aria-hidden="true"></i>
     <div class="popup-img">
       <img class="img-block" src="${project.image}" alt="#">
@@ -58,15 +57,13 @@ let projectCards = [
     </div>
   </div>`;
   
-
-  const popupClose = document.querySelector('.popup-close-btn');
-
-  popButton.forEach((element) => {
-    element.addEventListener('click', () => {
-      popupSection.classList.remove('d-none');
-    });
+const popupClose = document.querySelector('.popup-close-btn');
+popButton.forEach((element) => {
+  element.addEventListener('click', () => {
+    popupSection.classList.remove('d-none');
   });
+});
 
-  popupClose.addEventListener('click', () => {
-    popupSection.classList.add('d-none');
-  });
+popupClose.addEventListener('click', () => {
+  popupSection.classList.add('d-none');
+});
